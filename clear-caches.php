280829c012232213f2549cd5504b29963f0d6bea
mrschlei <mrschlei@umich.edu>
2018-03-06 11:25:12 -0500

    cache clear jazz

---
+++ clear-caches.php
@@ -0,0 +1,17 @@
+<?php
+
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+$autoloader = require_once 'autoload.php';
+
+$kernel = new DrupalKernel('prod', $autoloader);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
+
+drupal_flush_all_caches();
+?>
